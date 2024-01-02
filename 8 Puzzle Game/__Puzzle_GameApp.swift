a9ce7e744e66c501a601faa9f0038403ce394974
SabalKatuwal <sabal.katuwal1@gmail.com>
2024-01-02 17:02:02 +0545

    Initial Commit

---
+++ 8 Puzzle Game/__Puzzle_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  __Puzzle_GameApp.swift
+//  8 Puzzle Game
+//
+//  Created by Sabal on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct __Puzzle_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
